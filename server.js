0a4f953ed66be145dccc8b6a25509692089b35e4
Naveenrf <naveen@rightfunds.com>
2022-04-09 10:59:33 +0530

    ceck listen

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 
 const app = require('./app');
-app.listen( process.env.PORT || 3000, () => {
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+let listen = app.listen( process.env.PORT || 3000, () => {
+    console.log("Express server listening on port %d in %s mode", listen.address().port, app.settings.env);
     console.log("Server listen");
 })
 // 3306
